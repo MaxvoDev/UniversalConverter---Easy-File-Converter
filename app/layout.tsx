f81c61692b10938e24f3a2ae2850286e81e14534
Maxvo <maxvo25@gmail.com>
2024-01-30 22:29:46 +1030

    fix error text

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import GA from '@/components/GA';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Modifio - Free Unlimited File Converter',
-  description: `Unleash your creativity with Modifio – the ultimate online tool for
+  title: 'Universal Converter - Free Unlimited File Converter',
+  description: `Unleash your creativity with Universal Converter – the ultimate online tool for
   unlimited and free multimedia conversion. Transform images, audio, and
   videos effortlessly, without restrictions. Start converting now and
   elevate your content like never before!`,
